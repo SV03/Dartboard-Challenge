760dc3c7eec88288e3ae8a68708653c5031b0f0a
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-13 15:29:04 +0000

    Draw ground truth rectangles

--- task1/subtask1.py
+++ task1/subtask1.py
@@ -1,20 +1,32 @@
-import numpy as np
 import cv2
+import json 
+
+image_name = "dart15.jpg"
+image_path = "in/{}".format(image_name)
+img = cv2.imread(image_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Draw ground truth
+
+json_string = open('faces.json').read()
+images_to_faces = json.loads(json_string)
+
+faces_list = images_to_faces[image_name]
+for faces_coordinates in faces_list:
+  x1 = faces_coordinates[0]
+  y1 = faces_coordinates[1]
+  x2 = faces_coordinates[2]
+  y2 = faces_coordinates[3]
+  img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# Cascade Detection
 
 face_cascade = cv2.CascadeClassifier('frontalface.xml')
 
-
-img = cv2.imread('in/dart4.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+for (x, y, w, h) in faces:
+  img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-
-
-cv2.imshow('img',img)
+cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
