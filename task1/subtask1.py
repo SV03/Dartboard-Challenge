b4769d3a1c499a529137e3afc4e2b764428b0f78
Sarath Vaman <sarath@eduroam-168-146.nomadic.bris.ac.uk>
2019-11-13 15:45:11 +0000

    Save Img Function

--- task1/subtask1.py
+++ task1/subtask1.py
@@ -28,5 +28,6 @@ for (x, y, w, h) in faces:
   img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 cv2.imshow('img', img)
+cv2.imwrite('GT_dart15.png',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
