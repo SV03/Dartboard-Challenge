66494aa0d1d990364487efce1549788a932b5180
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-21 19:43:30 +0000

    Extract duplicated code in "util" module

--- task1/subtask1.py
+++ task1/subtask1.py
@@ -1,89 +1,63 @@
 import cv2
-import json
+import sys
+import os
+PARENT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PARENT_PATH); import util
 
-def load_ground_truth(ground_truth_file):
-  json_string = open(ground_truth_file).read()
-  return json.loads(json_string)
+if __name__ == '__main__':
+  image_path = sys.argv[1]
+  image_name = image_path.split('/')[-1]
+  print(f"Opening {image_name}")
+  image = cv2.imread(image_path)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def rectangle_area(p1, p2):
-  return (p2[0] - p1[0] + 1) * (p2[1] - p1[1] + 1)
+  images_to_faces = util.load_ground_truth("faces.json")
+  ground_truth_faces = images_to_faces[image_name]
+  number_of_faces = len(ground_truth_faces)
+  print("Number of faces:", number_of_faces)
 
-def overlap(p1, p2, q1, q2):
-  return (p1[0] < q2[0] and p2[0] > q1[0] and
-          p1[1] < q2[1] and p2[1] > q1[1])
+  face_cascade = cv2.CascadeClassifier('frontalface.xml')
+  detected_faces = face_cascade.detectMultiScale(gray, 1.1, 1, 0, (50, 50), (500, 500))
+  number_of_detections = len(detected_faces)
+  print("Number of detections:", number_of_detections)
 
-def intersection_over_union(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2):
-  ground_truth_area = rectangle_area(ground_truth_p1, ground_truth_p2)
-  detected_area = rectangle_area(detected_p1, detected_p2)
-  if (overlap(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2)):
-    intersection_p1 = (max(ground_truth_p1[0], detected_p1[0]), max(ground_truth_p1[1], detected_p1[1]))
-    intersection_p2 = (min(ground_truth_p2[0], detected_p2[0]), min(ground_truth_p2[1], detected_p2[1]))
-    intersection_area = rectangle_area(intersection_p1, intersection_p2)
-  else:
-    intersection_area = 0
-  union_area = ground_truth_area + detected_area - intersection_area
-  return intersection_area / union_area
+  for faces_coordinates in ground_truth_faces:
+    x1 = faces_coordinates[0]
+    y1 = faces_coordinates[1]
+    x2 = faces_coordinates[2]
+    y2 = faces_coordinates[3]
+    image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-def show_image(image, title="Image"):
-  cv2.imshow(title, image)
-  cv2.moveWindow(title, 0, 20);
-  cv2.waitKey()
-  cv2.destroyAllWindows()
-
-# Main
-
-image_name = "dart15.jpg"
-image_path = "in/{}".format(image_name)
-image = cv2.imread(image_path)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-images_to_faces = load_ground_truth("faces.json")
-ground_truth_faces = images_to_faces[image_name]
-number_of_faces = len(ground_truth_faces)
-print("Number of faces:", number_of_faces)
-
-face_cascade = cv2.CascadeClassifier('frontalface.xml')
-detected_faces = face_cascade.detectMultiScale(gray, 1.1, 1, 0, (50, 50), (500, 500))
-number_of_detections = len(detected_faces)
-print("Number of detections:", number_of_detections)
-
-for faces_coordinates in ground_truth_faces:
-  x1 = faces_coordinates[0]
-  y1 = faces_coordinates[1]
-  x2 = faces_coordinates[2]
-  y2 = faces_coordinates[3]
-  image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-for (x, y, w, h) in detected_faces:
-  image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-threshold = 0.5
-succeded_detections = 0
-i = 0
-
-for face_coordinate in ground_truth_faces:
-  ground_truth_p1 = (face_coordinate[0], face_coordinate[1])
-  ground_truth_p2 = (face_coordinate[2], face_coordinate[3])
-  # print("Ground truth", ground_truth_p1, ground_truth_p2)
   for (x, y, w, h) in detected_faces:
-    detected_p1 = (x, y)
-    detected_p2 = (x + w, y + h)
-    iou = intersection_over_union(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2)
-    # print(detected_p1, detected_p2, iou)
-    if(iou >= threshold):
-      succeded_detections += 1
+    image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-print("True Positives:", succeded_detections)
-false_positives = number_of_detections - succeded_detections
-print("False Positives:", false_positives)
-recall = succeded_detections / number_of_faces # Also called sensitivity and recall
-print("True Positive Rate: {} / {} = ".format(succeded_detections, number_of_faces), recall) 
-precision = succeded_detections / (succeded_detections + false_positives)  # Also called Positive Predictive Value (PPV)
-print("Precision:", precision)
-f1 = (2 * (recall * precision)) / (recall + precision)
-print("F1:", f1)
+  threshold = 0.5
+  succeded_detections = 0
+  i = 0
 
-labeled_file_name = image_name.replace("dart", "out/labeled")
-cv2.imwrite(labeled_file_name, image)
+  for face_coordinate in ground_truth_faces:
+    ground_truth_p1 = (face_coordinate[0], face_coordinate[1])
+    ground_truth_p2 = (face_coordinate[2], face_coordinate[3])
+    # print("Ground truth", ground_truth_p1, ground_truth_p2)
+    for (x, y, w, h) in detected_faces:
+      detected_p1 = (x, y)
+      detected_p2 = (x + w, y + h)
+      iou = util.intersection_over_union(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2)
+      # print(detected_p1, detected_p2, iou)
+      if(iou >= threshold):
+        succeded_detections += 1
 
-# show_image(image)
+  print("True Positives:", succeded_detections)
+  false_positives = number_of_detections - succeded_detections
+  print("False Positives:", false_positives)
+  recall = succeded_detections / number_of_faces # Also called sensitivity and recall
+  print("True Positive Rate: {} / {} = ".format(succeded_detections, number_of_faces), recall) 
+  precision = succeded_detections / (succeded_detections + false_positives)  # Also called Positive Predictive Value (PPV)
+  print("Precision:", precision)
+  f1 = (2 * (recall * precision)) / (recall + precision)
+  print("F1:", f1)
+
+  # labeled_file_name = image_name.replace("dart", "out/labeled")
+  # cv2.imwrite(labeled_file_name, image)
+
+  util.show_image(image)
---
+++ util.py
@@ -0,0 +1,31 @@
+import cv2
+import json
+
+def rectangle_area(p1, p2):
+  return (p2[0] - p1[0] + 1) * (p2[1] - p1[1] + 1)
+
+def overlap(p1, p2, q1, q2):
+  return (p1[0] < q2[0] and p2[0] > q1[0] and
+          p1[1] < q2[1] and p2[1] > q1[1])
+
+def intersection_over_union(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2):
+  ground_truth_area = rectangle_area(ground_truth_p1, ground_truth_p2)
+  detected_area = rectangle_area(detected_p1, detected_p2)
+  if (overlap(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2)):
+    intersection_p1 = (max(ground_truth_p1[0], detected_p1[0]), max(ground_truth_p1[1], detected_p1[1]))
+    intersection_p2 = (min(ground_truth_p2[0], detected_p2[0]), min(ground_truth_p2[1], detected_p2[1]))
+    intersection_area = rectangle_area(intersection_p1, intersection_p2)
+  else:
+    intersection_area = 0
+  union_area = ground_truth_area + detected_area - intersection_area
+  return intersection_area / union_area
+
+def show_image(image, title="Image"):
+  cv2.imshow(title, image)
+  cv2.moveWindow(title, 0, 20);
+  cv2.waitKey()
+  cv2.destroyAllWindows()
+
+def load_ground_truth(ground_truth_file):
+  json_string = open(ground_truth_file).read()
+  return json.loads(json_string)
