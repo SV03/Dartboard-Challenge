2bf3f7c8aeed99bde83c43c1da2a3e9935c387c3
SV03 <sarathvaman98@gmail.com>
2019-11-22 21:10:55 +0000

    Upper bound image scaling function

--- task3/hough_detector.py
+++ task3/hough_detector.py
@@ -16,6 +16,8 @@ def gradient_direction(image):
       direction[row, col] = math.atan2(gradient_y[row, col], gradient_x[row, col])
   return cv2.normalize(direction, normalized, 0, 255, cv2.NORM_MINMAX)
 
+
+
 if __name__ == '__main__':
   image_path = sys.argv[1]
   image_name = image_path.split('/')[-1]
@@ -23,13 +25,19 @@ if __name__ == '__main__':
   image = cv2.imread(image_path)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-  laplacian = cv2.Laplacian(gray, cv2.CV_64F, ksize=3)
+  # rotated_gray = cv2.rotate(resized_gray, cv2.ROTATE_90_CLOCKWISE)
+
+  resized_gray = util.image_bounds(gray, 600)
+
+  laplacian = cv2.Laplacian(resized_gray, cv2.CV_64F, ksize=3)
   edges = util.segment_by_threshold(laplacian, threshold = 250)
   labeled_file_name = image_name.replace("dart", "out/edges")
   cv2.imwrite(labeled_file_name, edges)
   
-  gradient_direction = gradient_direction(gray)
+  gradient_direction = gradient_direction(resized_gray)
   labeled_file_name = image_name.replace("dart", "out/gradient_dir")
   cv2.imwrite(labeled_file_name, gradient_direction)
 
   # util.show_image(gradient_direction)
+
+
