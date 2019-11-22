f856dd4b6889445281edb55a2a6578bbc40ac572
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-22 13:23:36 +0000

    Hough Transform Lines boilerplate

---
+++ task3/hough_transform_lines.py
@@ -0,0 +1,10 @@
+class HoughTransformLines(object):
+  def __init__(self):
+    print("initializing a HTL object")
+
+  def hello(self):
+    return "Hello World"
+
+if __name__ == "__main__":
+  htl = HoughTransformLines()
+  print(htl.hello())
