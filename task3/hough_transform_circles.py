20dda47a5bc97fdb6b67fedb4996e4fe979338d5
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-26 00:59:16 +0000

    Clean Hough Transform Circles

--- task3/hough_transform_circles.py
+++ task3/hough_transform_circles.py
@@ -9,20 +9,24 @@ import util
 import image_processing as ip
 
 class HoughTransformCircles(object):
-  def __init__(self, magnitude, direction, min_radius, max_radius):
-    self.magnitude = magnitude
-    self.direction = direction
+  def __init__(self, image, min_radius = 30, max_radius = 100):
+    self.image = image
+    self.height = image.shape[0]
+    self.width = image.shape[1]
     self.min_radius = min_radius
     self.max_radius = max_radius
-    self.height = magnitude.shape[0]
-    self.width = magnitude.shape[1]
+    self.gradient = None
+    self.direction = None
+    self.h_space = None
 
   def process_space(self, threshold):
+    self.__process_gradient_magnitude()
+    self.__process_gradient_direction()
     possible_radious = self.max_radius - self.min_radius
-    h_space = np.zeros([self.height, self.width, possible_radious])
+    self.h_space = np.zeros([self.height, self.width, possible_radious])
     for row in range(self.height):
       for col in range (self.width):
-        if self.magnitude[row][col] > threshold:
+        if self.gradient[row][col] > threshold:
           for r in range (possible_radious):
             radius = int(self.min_radius + r)
             direction_in_radians = (self.direction[row][col] * 2 * np.pi) / 255
@@ -31,20 +35,19 @@ class HoughTransformCircles(object):
 
             y0 = row - b
             x0 = col - a
-            if (y0 >= 0 and x0 >= 0 and y0 < self.height and x0 < self.width):
-              h_space[y0][x0][r] += 1
+            if (self.inside_image_scope(y = y0, x = x0)):
+              self.h_space[y0][x0][r] += 1
 
             y0 = row + b
             x0 = col + a
-            if (y0 >= 0 and x0 >= 0 and y0 < self.height and x0 < self.width):
-              h_space[y0][x0][r] += 1
-    self.h_space = h_space
+            if (self.inside_image_scope(y = y0, x = x0)):
+              self.h_space[y0][x0][r] += 1
     return self.h_space
   
   def squash_space(self, scale):
     return np.sum(self.h_space, axis=2) * scale
 
-  def detect_circles(self, threshold=25):
+  def detect_circles(self, threshold=20):
     detections = []; circles = []
     for row in range(self.h_space.shape[0]):
       for col in range(self.h_space.shape[1]):
@@ -57,6 +60,18 @@ class HoughTransformCircles(object):
         circles.append((row, col, radius))
     return circles
 
+  def inside_image_scope(self, x, y):
+    return y >= 0 and x >= 0 and y < self.height and x < self.width
+
+  def __process_gradient_magnitude(self):
+    self.gradient = ip.gradient_magnitude(gray)
+  # cv2.imwrite(f'preprocess/grad_{image_name}', self.gradient)
+
+  def __process_gradient_direction(self):
+    self.direction = ip.gradient_direction(gray)
+  # cv2.imwrite(f'preprocess/dir_{image_name}', self.direction)
+
+
 if __name__ == "__main__":
   image_path = sys.argv[1]
   image_name = image_path.split('/')[-1]
@@ -66,27 +81,17 @@ if __name__ == "__main__":
   image = ip.resize_with_aspect_ratio(image, max_side=150)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-  edges = ip.gradient_magnitude(gray)
-  # cv2.imwrite(f'preprocess/grad_{image_name}', edges)
-
   threshold = 80
-  gradient = ip.segment_by_threshold(edges, threshold=threshold)
-  cv2.imwrite(f'preprocess/grad_thre{image_name}', gradient)
-
-  grad_direction = ip.gradient_direction(gray)
-  cv2.imwrite(f'preprocess/dir_{image_name}', grad_direction)
-
-  htc = HoughTransformCircles(gradient, grad_direction, min_radius=30, max_radius=150)
-
+  htc = HoughTransformCircles(gray, min_radius=30, max_radius=100)
   h_space = htc.process_space(threshold=threshold)
   h_space_2d = htc.squash_space(scale=1)
-  
+  # cv2.imwrite(f'h_space/circles_{image_name}', h_space_2d)
+
   circles = htc.detect_circles(threshold=20)
   for row, col, radius in circles:
     p0 = (col - radius, row - radius)
     p1 = (col + radius, row + radius)
     image = cv2.rectangle(image, p0, p1, (0, 255, 0), 1)
 
-  # cv2.imwrite(f'h_space/circles_{image_name}', h_space_2d)
   cv2.imwrite(f'out/circles_{image_name}', image)
   util.show_image(image)
