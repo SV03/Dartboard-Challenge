78389c480f931d08704afae92f0c104a05fbac88
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-26 01:10:23 +0000

    Pseudocode in hough detector

--- task3/hough_detector.py
+++ task3/hough_detector.py
@@ -8,16 +8,6 @@ sys.path.append(PARENT_PATH);
 import util
 import image_processing as ip
 
-def gradient_direction(image):
-  direction = np.empty(image.shape)
-  normalized = np.empty(direction.shape)
-  gradient_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-  gradient_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
-  for row in range(image.shape[0]):
-    for col in range(image.shape[1]):
-      direction[row, col] = math.atan2(gradient_y[row, col], gradient_x[row, col])
-  return cv2.normalize(direction, normalized, 0, 255, cv2.NORM_MINMAX)
-
 if __name__ == '__main__':
   image_path = sys.argv[1]
   image_name = image_path.split('/')[-1]
@@ -25,52 +15,42 @@ if __name__ == '__main__':
   image = cv2.imread(image_path)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-  max_side = 300
-  image = ip.resize_with_aspect_ratio(image, max_side=max_side)
+  # max_side = 300
+  # image = ip.resize_with_aspect_ratio(image, max_side=max_side)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
   cascade_file = '../task2/dartcascade/cascade.xml'
   vj_detections = util.viola_jones_detection(cascade_file, gray)
 
+  # threshold = 80
+  # confirmed_detections = []
   for x, y, width, height in vj_detections:
     image = cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)
-    # extract 
-    # magnitude = ip.gradient_magnitude()
-    # direction = 
-    # htc = HougTransformCirle(magnitude, direction)
-    # htc.process_space()
-    # circles htc.detect_circles( )
+    # detection_region = CROP THE DETECTION REGION
+    # htc = HougTransformCirle(detection_region)
+    # htc.process_space(threshold=threshold)
+    # circles = htc.detect_circles()
     # if (len((circles) >= 1):
       # htl = HougTransformLines()   # 4 -HT Lines
       # if (htl.number_of_lines()):
-        # Confirm it is a dartboard
-        # else:
-        # REJECT DETECTION
+        # confirmed_detections.append(detection)
+  
+  # for x, y, width, height in confirmed_detections:
+    # image = cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+  # Draw ground truth
+  
+  # Print Performance Report
+  # cv2.imwrite(f'out/{image_name}', gradient)
+  util.show_image(image)
 
 
-  # cv2.imwrite(f'preprocess/grad_{image_name}', magnitude)
-
-  # threshold = 80
   # gradient = ip.segment_by_threshold(magnitude, threshold=threshold)
   # cv2.imwrite(f'preprocess/grad_thre{image_name}', gradient)
 
   # grad_direction = ip.gradient_direction(gray)
   # cv2.imwrite(f'preprocess/dir_{image_name}', grad_direction)
   # 1 resize for efficiency
-  
-  # Show image with detections
-  # Show Performance Report
 
-  # resized_gray = util.image_bounds(gray, 600)
-
-  # edges = util.segment_by_threshold(laplacian, threshold = 250)
-  # labeled_file_name = image_name.replace("dart", "out/edges")
-  # cv2.imwrite(labeled_file_name, edges)
-  
-  # gradient_direction = gradient_direction(resized_gray)
-  # labeled_file_name = image_name.replace("dart", "out/gradient_dir")
-  # cv2.imwrite(labeled_file_name, gradient_direction)
-
-  util.show_image(image)
 
 
--- util.py
+++ util.py
@@ -2,9 +2,6 @@ import cv2
 import json
 import numpy as np
 
-def abs(n):
-  return n if n >= 0 else -n
-
 def rectangle_area(p1, p2):
   return (p2[0] - p1[0] + 1) * (p2[1] - p1[1] + 1)
 
@@ -34,91 +31,6 @@ def load_ground_truth(ground_truth_file):
   json_string = open(ground_truth_file).read()
   return json.loads(json_string)
 
-def segment_by_threshold(image, threshold):
-  result = np.empty(image.shape)
-  for row in range(image.shape[0]):
-    for col in range(image.shape[1]):
-      if image[row, col] >= threshold:
-        result[row, col] = image[row, col]
-  return result
-
-def normalize(image, min_value, max_value):
-  normalized = np.empty(image.shape)
-  return cv2.normalize(image, normalized, min_value, max_value, cv2.NORM_MINMAX)
-
-def image_bounds(image, pixel_value):
-  width = np.size(image, 0); height = np.size(image,1)
-  pixel_bound_x = pixel_value ; pixel_bound_y = pixel_value; scale_percent = 1
-
-  if (width > pixel_bound_x or height > pixel_bound_y):
-    ratioX = (pixel_bound_x/width)
-    ratioY = (pixel_bound_y/height)
-    if (width > height):
-      scale_percent = ratioX
-    else: 
-      scale_percent = ratioY
-
-  width = int(image.shape[1] * scale_percent)
-  print("New width:", width)
-  height = int(image.shape[0] * scale_percent)
-  print("New height:", height)
-
-  dim = (width, height)
-  resized_gray = cv2.resize(image, dim, cv2.INTER_AREA)
-
-  return resized_gray
-
 def viola_jones_detection(cascade_file, gray_image):
   cascade_classifier = cv2.CascadeClassifier(cascade_file)
   return cascade_classifier.detectMultiScale(gray_image, 1.1, 2, 0, (50, 50), (500, 500))
-
-def viola_jones_cascade(cascade_file, image_name,image,resized_gray):
-
-  #Multicascade  Detection
-  images_to_dartboards = load_ground_truth("../task2/dartboards.json")
-  ground_truth_dartboards = images_to_dartboards[image_name]
-  number_of_dartboards = len(ground_truth_dartboards)
-  print("Number of dartboards:", number_of_dartboards)
-  
-  vj_dartboards = cv2.CascadeClassifier('../task2/dartcascade/cascade.xml')
-  detected_dartboards = vj_dartboards.detectMultiScale(resized_gray, 1.1, 2, 0, (50, 50), (500, 500))
-  number_of_detections = len(detected_dartboards)
-  print("Number of detections:", number_of_detections)
-  
-
-  for (x1, y1, x2, y2) in ground_truth_dartboards:
-    image = cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-  for (x, y, w, h) in detected_dartboards:
-    image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-  threshold = 0.5
-  succeded_detections = 0
-  i = 0
-
-  for (x1, y1, x2, y2) in ground_truth_dartboards:
-    ground_truth_p1 = (x1, y1)
-    ground_truth_p2 = (x2, y2)
-    for (x, y, w, h) in detected_dartboards:
-      detected_p1 = (x, y)
-      detected_p2 = (x + w, y + h)
-      iou = intersection_over_union(ground_truth_p1, ground_truth_p2, detected_p1, detected_p2)
-      if(iou >= threshold):
-        succeded_detections += 1
-
-  print("True Positives:", succeded_detections)
-  false_positives = number_of_detections - succeded_detections
-  print("False Positives:", false_positives)
-  recall = succeded_detections / number_of_dartboards # Also called sensitivity and recall
-  print("True Positive Rate: {} / {} = ".format(succeded_detections, number_of_dartboards), recall) 
-  precision = succeded_detections / (succeded_detections + false_positives)  # Also called Positive Predictive Value (PPV)
-  print("Precision:", precision)
-  f1 = (2 * (recall * precision)) / (recall + precision)
-  print("F1:", f1)
-
-  # htc = HoughTransformCircles(mag, dir)
-  # htc.process_space()
-  # circles = htc.detect_circles()
-  
-
-
